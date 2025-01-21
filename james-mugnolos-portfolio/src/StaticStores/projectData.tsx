533fd481e23e7b07847439e267efbe2fad10f5a2
JamesMugnolo <mugnolojames@gmail.com>
2025-01-21 15:06:23 -0600

    added a project to the portfolio

--- james-mugnolos-portfolio/src/StaticStores/projectData.tsx
+++ james-mugnolos-portfolio/src/StaticStores/projectData.tsx
@@ -20,7 +20,7 @@ export const ProjectData: IProjectDataLayout[] = [
     description:
       "This is a simple full stack web app to monitor a user's todos. It has an angular frontend, a spring boot backend and PostgreSQL database. This app is also fully containerized through docker and uses Cloudflare tunnels to host the services through docker.",
     githubLink: "https://github.com/JamesMugnolo/todo-list",
-    deployLink: "https://nginx.jmtodolist.com",
+    deployLink: "https://todos.jm-projects.com/",
   },
   {
     id: 3,
@@ -29,5 +29,13 @@ export const ProjectData: IProjectDataLayout[] = [
       "This project is my portfolio which you are currently on! My portfolio is a static React and Typescript based website that's hosted through Netlify.",
     githubLink: "https://github.com/JamesMugnolo/Portfolio",
     deployLink: null,
+  },
+  {
+    id: 4,
+    name: "Nerd Cache Catalog",
+    description:
+      "NerdCache is a full stack web application that allows a user to manage their video game, book, and movie collections. The frontend uses React.js with MUI material library for some styling and Redux.js for ephemeral data storage. The backend uses Node.js to process restful API calls for getting and formatting data from external APIs as well as storing and modifying user and item data in a Postgres database.The website is orchestrated with docker containers and hosted through cloudflare.",
+    githubLink: "https://github.com/JamesMugnolo/My-Catalog-Manager",
+    deployLink: "https://nerdcashe.jm-projects.com/",
   },
 ];
